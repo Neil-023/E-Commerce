d308021645f4165a57ce38c158f827efd6056dfc
Neil <neiljohn.nj23@gmail.com>
2025-11-07 15:34:57 +0800

    Github Pages Deploy

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import { BrowserRouter, Routes, Route, useParams } from 'react-router-dom';
+import { HashRouter, Routes, Route, useParams } from 'react-router-dom';
 import MainPage from './MainPage.jsx';
 import AddProductPage from './AddProductPage';
 import CartPage from './CartPage';
